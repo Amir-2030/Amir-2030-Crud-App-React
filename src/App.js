ac2e006413d9438e9fd29535b1dfbede1961c6f7
Amir-2030 <mo.amir1973@gmail.com>
2022-02-24 19:39:23 +0200

    weather

--- src/App.js
+++ src/App.js
@@ -1,72 +1,74 @@
-import CourseForm from "./Component/CourseForm.js";
-import CourseList from "./Component/CourseList.js";
-import React , { Component ,  } from 'react';
+import React, { Component } from 'react';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
-class  App extends Component {
+
+const API_KEY = "3504842dd20127b111dd6f23135a79c0";
+
+// http://api.openweathermap.org/data/2.5/weather?q=cairo,egypt&appid=e36ed364400282e43250b6c4c0274d44
+class App extends Component {
+
   state = {
-    courses : [
-      {name : "HTML"},
-      {name : "CSS"},
-      {name : "jQuery"},
-    ] ,
-    current : ''
+    tempreature: '',
+    city: '',
+    country: '',
+    humidity: '',
+    description: '',
+    error : ''
   }
 
+  getWeather = async (e) => {
 
-  // UpdateCourse
-  updateCourse = (e) => {
-    this.setState({
-      current: e.target.value
-    })
-  }
-
-  // addCourse
-  addCourse = (e) => {
-    e.preventDefault();
-    let current = this.state.current;
-    let courses = this.state.courses;
-    courses.push({name: current})
-    this.setState({
-      courses,
-      current: ''
-    })
-  }
-
-  // deleteCourse
-  deleteCourse = (index) => {
-    let courses = this.state.courses;
-    courses.splice(index , 1);
-    this.setState({
-      courses
-    })
-  }
-
-  // editCourse 
-  editCourse = (index,value) => {
-    let courses = this.state.courses;
-    let course = courses[index];
-    course['name'] = value;
-    this.setState({
-      courses
-    })
-  }
+    e.preventDefault() 
+    //For stop reloaing
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`)
+    const data = await api.json();
   
-  render() {
-    const {courses} = this.state;
-    const courseList = courses.map( (course , index) => {
-      return <CourseList details={course} key={index} index={index} update={this.handleChange} deleteCourse={this.deleteCourse} editCourse={this.editCourse}/>
-    })
-    return (
-      <section className="App">
-        
-        <h2>Add Course</h2>
-        <CourseForm current={this.state.current} updateCourse={this.updateCourse} addCourse={this.addCourse}/> 
-        <ul>{courseList}</ul>
+    if(city && country) {
+      this.setState({
+        temperature:data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error : ''
+      })
+    } else {
+      this.setState({
+        temperature: '',
+        city: '',
+        country: '',
+        humidity: '',
+        description: '',
+        error : 'Please Enter Data'
+      })
+    }
+  }
 
-      </section>
+
+
+
+
+
+  render() {
+    return (
+      <div className="wrapper">
+          <div className="form-container">
+            <Form getWeather={this.getWeather} />
+            <Weather 
+              temperature={this.state.temperature} 
+              humidity={this.state.humidity}
+              city={this.state.city}
+              country={this.state.country}
+              description={this.state.description}
+              error={this.state.error}
+            />
+          </div>
+      </div>
     );
   }
 }
-
 
 export default App;
